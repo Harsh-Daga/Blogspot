df338c7693c67ed93fec2aed3f9182814979a8a6
Harsh-Daga <hs108699@gmail.com>
2023-08-07 23:13:32 +0530

    initial commit

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter, Roboto, Poppins } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeProvider } from "@/context/ThemeContext";
+import AuthProvider from "@/components/AuthProvider/AuthProvider";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["200","400","600","800"] });
+
+export const metadata = {
+  title: "Blogspot",
+  description: "This is the description",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <ThemeProvider>
+          <AuthProvider>
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
